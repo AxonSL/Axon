dd8bfd11694189d8faf64dd32c1f694647059fe2
GrafDimenzio <66207264+GrafDimenzio@users.noreply.github.com>
2024-05-26 14:34:10 +0200

    Added Meta and fixed Central Server Auth

---
+++ Axon.Client/Meta/MetaAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using Il2CppCommandSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axon.Client.Meta;
+
+public class MetaAnalyzer
+{
+    public void AnalyzeAssembly(Assembly assembly)
+    {
+        foreach (var type in assembly.GetTypes())
+        {
+            AnalyzeType(type);
+        }
+    }
+
+    public void AnalyzeType(Type type)
+    {
+        //write later with event system
+        if (type.GetCustomAttribute<Automatic>() == null) return;
+    }
+}
--- Axon.Client/Mod.cs
+++ Axon.Client/Mod.cs
@@ -8,6 +8,8 @@ using MelonLoader;
 using Axon.Client;
 using System.IO;
 using Axon.Client.AssetBundle;
+using Il2Cpp;
+using Axon.Client.Meta;
 
 [assembly: MelonInfo(typeof(Mod), "Axon", "0.0.1", "Dimenzio & Tiliboyy")]
 [assembly: MelonGame("Northwood", "SCPSL")]
@@ -19,13 +21,23 @@ public class Mod : MelonMod
 
     public static AssetBundleManager AssetBundleManager { get; private set; }
 
+    public static MetaAnalyzer MetaAnalyzer { get; private set; }
+
     public override void OnInitializeMelon()
     {
+        CustomNetworkManager.Modded = true;
+        Il2CppCentralAuth.CentralAuthManager.NoAuthStartupArg = "yes?";
+
         HarmonyInstance.PatchAll();
+
         Instance = this;
+
         AssetBundleManager = new AssetBundleManager();
         AssetBundleManager.Init();
+
+        MetaAnalyzer = new MetaAnalyzer();
+        MetaAnalyzer.AnalyzeAssembly(MelonAssembly.Assembly);
+
         LoggerInstance.Msg("Axon Loaded");
-        LoggerInstance.Msg(Directory.GetCurrentDirectory());
     }
 }
