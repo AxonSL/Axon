330f3ae258608119bc05e02add33029ed708b85a
GrafDimenzio <66207264+GrafDimenzio@users.noreply.github.com>
2024-05-26 10:49:46 +0200

    updated Newsmessage

--- Axon.Client/Patches/News/NewsLoaderStartPatch.cs
+++ Axon.Client/Patches/News/NewsLoaderStartPatch.cs
@@ -18,11 +18,11 @@ public class NewsLoaderStartPatch
         __instance._announcements = new Il2CppSystem.Collections.Generic.List<NewsLoader.Announcement>();
         __instance._announcements.Clear();
         __instance._announcements.Add(new NewsLoader.Announcement(
-            $"<color=blue>Axon</color>",
+            $"<color=#ff3300>Axon</color>",
             "<b><size=20>Welcome to Axon, a SCP:SL modded version.</size></b>\n" +
             "\n<color=#ec0c02>Alpha Built</color>",
-            "25.05.24",
-            "https://scopesl.com", null));
+            "26.05.24",
+            "", null));
         __instance.ShowAnnouncement(0);
 
         return false;
