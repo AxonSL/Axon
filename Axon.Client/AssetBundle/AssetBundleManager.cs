33e31f05235296af4e0a721667b4ada19c2f325d
GrafDimenzio <66207264+GrafDimenzio@users.noreply.github.com>
2024-05-26 11:17:17 +0200

    implemented Asset Loading

---
+++ Axon.Client/AssetBundle/AssetBundleManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axon.Client.AssetBundle;
+
+public class AssetBundleManager
+{
+    private bool _initiated = false;
+
+    public const string AssetDirectoryName = "AssetBundles";
+    public string AssetDirectory { get; private set; }
+    public Dictionary<string, Il2CppAssetBundle> AssetBundles { get; private set; } = new();
+
+    public void Init()
+    {
+        if(_initiated) return;
+
+        var current = Directory.GetCurrentDirectory();
+        AssetDirectory = Path.Combine(current, AssetDirectoryName);
+        if (!Directory.Exists(AssetDirectory))
+            Directory.CreateDirectory(AssetDirectory);
+
+        LoadAssets();
+        _initiated = true;
+    }
+
+    private void LoadAssets()
+    {
+        foreach(var assetPath in Directory.GetFiles(AssetDirectory))
+        {
+            var asset = Il2CppAssetBundleManager.LoadFromFile(assetPath);
+            var fileName = Path.GetFileNameWithoutExtension(assetPath);
+            AssetBundles.Add(fileName, asset);
+        }
+    }
+}
--- Axon.Client/Mod.cs
+++ Axon.Client/Mod.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using HarmonyLib;
 using MelonLoader;
 using Axon.Client;
+using System.IO;
+using Axon.Client.AssetBundle;
 
 [assembly: MelonInfo(typeof(Mod), "Axon", "0.0.1", "Dimenzio & Tiliboyy")]
 [assembly: MelonGame("Northwood", "SCPSL")]
@@ -15,10 +17,15 @@ public class Mod : MelonMod
 {
     public static Mod Instance { get; private set; }
 
+    public static AssetBundleManager AssetBundleManager { get; private set; }
+
     public override void OnInitializeMelon()
     {
         HarmonyInstance.PatchAll();
         Instance = this;
+        AssetBundleManager = new AssetBundleManager();
+        AssetBundleManager.Init();
         LoggerInstance.Msg("Axon Loaded");
+        LoggerInstance.Msg(Directory.GetCurrentDirectory());
     }
 }
